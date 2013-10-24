0c5374ae26094e17c60dfd694f9caa7aa70bd5cc
JBassx <joseph.ciullo@libero.it>
2013-10-24 17:08:20 +0200

    Creato metodo per importare gli account

--- src/com/bugmenot/JBugMeNot.java
+++ src/com/bugmenot/JBugMeNot.java
@@ -3,10 +3,16 @@
  */
 package com.bugmenot;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
- * @author <a href="https://github.com/DavidePastore">DavidePastore</a>
+ * @author <a href="https://github.com/DavidePastore">DavidePastore</a> & <a href="https://github.com/JBassx"> Giuseppe  Ciullo</a>
  *
  */
 public class JBugMeNot {
@@ -41,12 +47,35 @@ public class JBugMeNot {
 		return BASE_URL;
 	}
 	
-	
-	
-	public static ArrayList<Account> getAllAccounts(String website){
-		
+	public static ArrayList<Account> getAllAccounts(String website) throws IOException
+	{
+		return getAllAccounts(website,"Mozilla");
 	}
 	
-	
+	public static ArrayList<Account> getAllAccounts(String website,String userAgent) throws IOException{
+		ArrayList<Account> accounts=null;
+		Document doc = Jsoup.connect("http://www.bugmenot.com/view/"+website).userAgent(userAgent).get();
+		Elements account_elements = doc.getElementsByClass("account");
+		for(Element account_element : account_elements)	{
+			Element tbody = account_element.select("table tbody").first();
+			if(Integer.parseInt(tbody.child(3).getElementsByTag("td").text().substring(0, 2))>=50){
+				Account account=new Account();
+				Elements tr_elements=tbody.children();
+				for(Element tr:tr_elements){
+					String th = tr.getElementsByTag("th").text();
+					String td = tr.getElementsByTag("td").text();
+					switch(th.toLowerCase()){
+						case "username"	:	account.setUsername(td);break;
+						case "password"	:	account.setPassword(td);break;
+						case "other"	:	account.setOther(td);break;
+						case "stats"	:	account.setStats(td.substring(0, 3));break;
+					}
+				}
+			}
+			else break;
+		}
+		return accounts;
+	}
+		
 
 }
